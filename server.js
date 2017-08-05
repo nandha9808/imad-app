a122ce55c25c1021bf4b30b744274cd0adb9481b
nandha9808 <30661793+nandha9808@users.noreply.github.com>
2017-08-05 04:12:04 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
- var article={
-     'articles':{
+ var articles={
+     'article':{
      title:"article",
      heading:"article one",
      content:`<p>
