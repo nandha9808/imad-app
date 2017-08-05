817bf39dcab6c9fc6988c0af225d5763c9efbf88
nandha9808 <30661793+nandha9808@users.noreply.github.com>
2017-08-04 23:49:24 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one',function(req,res){
-    res.send("article is reqested and served");
+app.get('/article',function(req,res){
+ res.sendFile(path.join(__dirname, 'ui', 'article.html'));
+   
 });
 app.get('/article-two',function(req,res){
     res.send("articletwo is reqested and served");
