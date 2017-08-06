f56eb328d8fea016908a87ef4ff4d722cb553f56
nandha9808 <30661793+nandha9808@users.noreply.github.com>
2017-08-05 23:31:10 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("demo").innerHTML="HI ALL!!";
+var img=document.getElementByID("d");
+img.onclick=function()
+{
+  img.style.Left="100px";  
+};
