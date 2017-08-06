2049f1cba45b6cffd5cf6e3ad4b94be1eed6cac4
nandha9808 <30661793+nandha9808@users.noreply.github.com>
2017-08-05 22:50:27 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,8 +83,8 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName',function(req,res){
  
-var articleName=req.params.articlename;
-res.send(createTemplate(article));
+var articleName=req.params.articleName;
+res.send(createTemplate(articles[articleName]));
 });
 
 
