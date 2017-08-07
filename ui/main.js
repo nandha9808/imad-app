f73dda5624c7fb8c4930758093664324c37fecf9
nandha9808 <30661793+nandha9808@users.noreply.github.com>
2017-08-07 08:18:35 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ var button=document.getElementById("counter");
 button.onclick=function(){
   counter=counter+1;
   var span=document.getElementById("span");
-  span.innerHTML=counter+1;
+  span.innerHTML=counter.toString();
 };
