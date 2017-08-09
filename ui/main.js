8f60a110cac0d893ca65b23289814f21282588dc
nandha9808 <30661793+nandha9808@users.noreply.github.com>
2017-08-09 08:19:58 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ button.onclick=function(){
             //submit
             var nameInput=document.getElementById("name");
             var name=nameInput.value;
-            var submit=document.getElementById("submit_btn");
+            var submit=document.getElementById("submit");
             submit.onclick=function(){
                 var names=['name1','name2','name3'];
                 var list='';
