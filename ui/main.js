3a28aa53fde75224cca1bbd768384a247b28e9b2
nandha9808 <30661793+nandha9808@users.noreply.github.com>
2017-08-09 08:15:35 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,17 @@ button.onclick=function(){
             request.open('GET','http://nandhaspmkumar.imad.hasura-app.io/counter',true);
             request.send(null);
             };
+            //submit
+            var nameInput=document.getElementById("name");
+            var name=nameInput.value;
+            var submit=document.getElementById("submit_btn");
+            submit.onclick=function(){
+                var names=['name1','name2','name3'];
+                var list='';
+                for(i=0;i<names.length;i++)
+                {
+                    list='<li>'+name+'</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
